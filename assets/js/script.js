c63a45b28d1c036560081c17946dea056a1ed849
dasilva <dasilva1807@hotmail.com>
2019-03-27 02:56:09 -0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+      var x = this.hash;
+      $("html, body").animate({
+        scrollTop: $(x).offset().top
+      }, 1000, function(){
+        window.location.hash = x;
+      });
+    }
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
